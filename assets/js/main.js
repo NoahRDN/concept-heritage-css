bdec57901ffae18211a8e8d0e0e0b1a179ad3507
Mike <noahmichaelrandrianah@gmail.com>
2024-09-11 06:35:51 +0300

    responsive en general index.html et wakanda.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// function limite de defilement reseaux sociaux
 window.addEventListener('scroll', function() {
     const header = document.querySelector('.header__reseau__sociaux');
     const formulaire = document.querySelector('#formulaire');
@@ -16,4 +17,42 @@ window.addEventListener('scroll', function() {
         header.style.top = '0px';
     }
 
+});
+
+// function pour le barre de navigation version mobile
+let lastScrollTop = 0;
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', function() {
+    let currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScrollTop > lastScrollTop) {
+
+        header.style.top = '-60px';
+    } else {
+
+        header.style.top = '0';
+    }
+
+    lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
+});
+
+
+// function boutton d'ouverture et fermeture menu burger
+document.addEventListener('DOMContentLoaded', function() {
+    const burgerBtn = document.getElementById('burger-btn');
+    const menuOverlay = document.getElementById('fullpage-menu');
+    let isMenuOpen = false;
+
+    burgerBtn.addEventListener('click', function() {
+        menuOverlay.classList.toggle('active');
+
+        if (!isMenuOpen) {
+            burgerBtn.src = '/assets/img/all-page/cross-svgrepo-com.svg';
+        } else {
+            burgerBtn.src = '/assets/img/all-page/burger-menu-svgrepo-com.svg';
+        }
+
+        isMenuOpen = !isMenuOpen;
+    });
 });
